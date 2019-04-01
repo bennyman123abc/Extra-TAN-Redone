d414c245520028329feaea14ddd201d7b0a30aee
bennyman123abc <bennyman123abc@hotmail.com>
2019-04-01 18:13:43 -0400

    Initial Commit

---
+++ src/main/java/com/bendevnull/ExtraTANRedone/MainClass.java
@@ -0,0 +1,31 @@
+package com.bendevnull.ExtraTANRedone;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID,
+	 name=Reference.MODNAME, 
+	 version=Reference.VERSION,
+	 acceptedMinecraftVersions=Reference.ACCEPTED_MINECRAFT_VERSIONS,
+	 dependencies=Reference.DEPENDENCIES)
+public class MainClass {
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+	
+}
---
+++ src/main/java/com/bendevnull/ExtraTANRedone/Reference.java
@@ -0,0 +1,11 @@
+package com.bendevnull.ExtraTANRedone;
+
+public class Reference {
+	
+	public static final String MODID = "extan-r";
+	public static final String MODNAME = "Extra TAN Redone";
+	public static final String VERSION = "1.12.2-1.0";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
+	public static final String DEPENDENCIES = "required-after:ToughAsNails";
+	
+}
